eedf189d9935e37db75a62e64bc71049944dde74
huy.duonglequang <huy.duonglequang@ncc.asia>
2025-03-04 01:42:28 +0000

    update structure build

--- src/modules/map/dto/map.dto.ts
+++ src/modules/map/dto/map.dto.ts
@@ -1,3 +1,6 @@
+import { Expose, Type } from 'class-transformer';
+import { IsOptional, IsString, IsUUID } from 'class-validator';
+
 export class CreateMapDto {
     readonly name: string;
     readonly map_key?: string;
@@ -6,6 +9,14 @@ export class CreateMapDto {
 }
 
 export class UpdateMapDto {
+    readonly id: string;
+    readonly name?: string|null;
+    readonly map_key?: string|null;
+    readonly width?: number|null;
+    readonly height?: number|null;
+}
+
+export class MapDetailDto {
     readonly id: string;
     readonly name?: string|null;
     readonly map_key?: string|null;
