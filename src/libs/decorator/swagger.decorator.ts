5280efa74404bbd75c3f4ffbc5aa609b9a582a79
huy.duonglequang <huy.duonglequang@ncc.asia>
2025-03-12 10:57:32 +0000

    Change the example payload swagger in API update user

--- src/libs/decorator/swagger.decorator.ts
+++ src/libs/decorator/swagger.decorator.ts
@@ -24,33 +24,33 @@ export function ApiUpdateUser() {
   return applyDecorators(
     ApiOperation({
       summary: 'Update user position and map',
-      description: 'This API allows updating the position (position_x, position_y) and the map (mapId) of a user.',
+      description: 'This API allows updating the position (position_x, position_y) and the map (map_id) of a user.',
     }),
     ApiBody({
       type: UpdateUserDto,
       examples: {
         example1: {
           summary: 'Update both map and position',
-          description: 'Updates the mapId and the user\'s position.',
+          description: 'Updates the map_id and the user\'s position.',
           value: {
-            mapId: 'b3f5eabc-8c45-4e6b-a1d6-5c748e6b6a1f',
+            map_id: 'b3f5eabc-8c45-4e6b-a1d6-5c748e6b6a1f',
             position_x: 100,
             position_y: 200,
           },
         },
         example2: {
           summary: 'Update only position',
-          description: 'Keeps the current mapId and only updates the position.',
+          description: 'Keeps the current map_id and only updates the position.',
           value: {
             position_x: 150,
             position_y: 250,
           },
         },
         example3: {
-          summary: 'Update only mapId',
+          summary: 'Update only map_id',
           description: 'Updates the map but keeps the current position.',
           value: {
-            mapId: 'a1d6b3f5-8c45-4e6b-5c74-8e6b6a1fa1d6',
+            map_id: 'a1d6b3f5-8c45-4e6b-5c74-8e6b6a1fa1d6',
           },
         },
       },
@@ -62,10 +62,10 @@ export function ApiUpdateUser() {
         example: {
           id: '1234',
           username: 'user123',
-          mapId: 'a1d6b3f5-8c45-4e6b-5c74-8e6b6a1fa1d6',
+          map_id: 'a1d6b3f5-8c45-4e6b-5c74-8e6b6a1fa1d6',
           position_x: 150,
           position_y: 250,
-          updatedAt: '2025-03-05T12:34:56.000Z',
+          updated_at: '2025-03-05T12:34:56.000Z',
         },
       },
     }),
