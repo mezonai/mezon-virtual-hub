2cd3597655db116817df18ffd7502cceefa94a7e
phi.phamlequoc <phi.phamlequoc@ncc.asia>
2025-08-18 11:52:14 +0700

    fix/admin-web: remove comment

--- admin/src/type/user/user.ts
+++ admin/src/type/user/user.ts
@@ -16,32 +16,3 @@ export type UserParams = {
   sort_by: string;
   order: SortOrder;
 };
-
-
-
-// export interface User {
-//   id: string;
-//   mezon_id: string;
-//   username: string;
-//   email: string;
-//   display_name: string;
-//   gold: number;
-//   diamond: number;
-//   gender: Gender;
-//   has_first_reward: boolean;
-//   created_at: string;
-//   updated_at: string;
-//   map: MapData;
-//   role: Role;
-// }
-
-// export interface MapData {
-//   id: string;
-//   name: string;
-//   map_key: string;
-//   default_position_x: number;
-//   default_position_y: number;
-//   is_locked: boolean;
-//   created_at: string;
-//   updated_at: string;
-// }
