950e387c0653faaf6f15c3d4d687212dc36288c4
Minh Nguyen Hoang Khanh <minh.nguyenhoangkhanh@ncc.asia>
2025-08-27 15:31:26 +0700

    feature/be/crud-reward-apis:  CRUD rewards apis

--- src/modules/reward/reward.controller.ts
+++ src/modules/reward/reward.controller.ts
@@ -1,8 +1,22 @@
 import { RequireAdmin } from '@libs/decorator';
-import { Controller } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ClsService } from 'nestjs-cls';
 import { RewardService } from './reward.service';
+import { CreateRewardDto } from './dto/reward.dto';
 
 @ApiTags('Reward')
 @Controller('rewards')
@@ -13,4 +27,35 @@ export class RewardController {
     private readonly rewardService: RewardService,
     private readonly cls: ClsService,
   ) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Get list all rewards',
+  })
+  async getAllRewards() {
+    return await this.rewardService.getAll();
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: 'Create a new reward',
+  })
+  async createRewardItem(@Body() payload: CreateRewardDto) {
+    return await this.rewardService.createReward(payload);
+  }
+
+  @Put(':reward_id')
+  @ApiOperation({
+    summary: 'Update a existed reward',
+  })
+  @ApiParam({
+    name: 'reward_id',
+    example: '91bea29f-0e87-42a5-b851-d9d0386ac32f',
+  })
+  async updateRewardItem(
+    @Body() payload: CreateRewardDto,
+    @Param('reward_id', ParseUUIDPipe) reward_id: string,
+  ) {
+    return await this.rewardService.updateReward(reward_id, payload);
+  }
 }
