693f227964a4aa603d45afcb7c1cecc1cd4adfcf
Minh Nguyen Hoang Khanh <minh.nguyenhoangkhanh@ncc.asia>
2025-09-11 09:07:48 +0000

    Update spin reward configuration

--- src/enum/game.enum.ts
+++ src/enum/game.enum.ts
@@ -5,3 +5,10 @@ export enum RewardSlotType {
   DIAMOND = 'diamond',
   NONE = 'none',
 }
+
+export enum RewardConfig {
+  LOW_COIN_MIN = 1,
+  LOW_COIN_MAX = 100,
+  HIGH_COIN_MIN = 101,
+  HIGH_COIN_MAX = 200,
+};
