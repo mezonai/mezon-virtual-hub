9afefaf81accc4b64953bf797285aafc32da2463
Minh Nguyen Hoang Khanh <minh.nguyenhoangkhanh@ncc.asia>
2025-10-02 11:51:07 +0700

    Update: Set is purchasable is false for upgrade cards

--- src/migrations/1758524807533-add-is-purchasable-into-item.ts
+++ src/migrations/1758524807533-add-is-purchasable-into-item.ts
@@ -1,3 +1,4 @@
+import { ItemCode } from '@enum';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddIsPurchasableIntoItem1758524807533
@@ -9,6 +10,16 @@ export class AddIsPurchasableIntoItem1758524807533
     await queryRunner.query(
       `ALTER TABLE "item" ADD "is_purchasable" boolean NOT NULL DEFAULT true`,
     );
+
+    await queryRunner.query(`
+      UPDATE "item"
+      SET "is_purchasable" = false
+      WHERE "item_code" IN (
+        '${ItemCode.RARITY_CARD_RARE}',
+        '${ItemCode.RARITY_CARD_EPIC}',
+        '${ItemCode.RARITY_CARD_LEGENDARY}'
+      )
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
