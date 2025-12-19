14fb881b878a22a0b80c022ed3452dfab5f36399
thyannguyentran <an.nguyentranthy@ncc.asia>
2025-12-19 20:12:29 +0700

    Update Limit Harvest For User

--- src/migrations/1766148942403-update-limit-harvest-for-user.ts
+++ src/migrations/1766148942403-update-limit-harvest-for-user.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class UpdateLimitHarvestForUser1766148942403 implements MigrationInterface {
     name = 'UpdateLimitHarvestForUser1766148942403'
 
-   public async up(queryRunner: QueryRunner): Promise<void> {
+  public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       ALTER TABLE "user_clan_stats"
       ALTER COLUMN "harvest_count" SET DEFAULT 100;
@@ -16,8 +16,17 @@ export class UpdateLimitHarvestForUser1766148942403 implements MigrationInterfac
 
     await queryRunner.query(`
       UPDATE "user_clan_stats"
-      SET "harvest_count" = 100,
-          "harvest_interrupt_count" = 100;
+      SET
+        "harvest_count" = CASE
+          WHEN "harvest_count" IS DISTINCT FROM 100 THEN 100
+          ELSE "harvest_count"
+        END,
+        "harvest_interrupt_count" = CASE
+          WHEN "harvest_interrupt_count" IS DISTINCT FROM 100 THEN 100
+          ELSE "harvest_interrupt_count"
+        END
+      WHERE "harvest_count" IS DISTINCT FROM 100
+         OR "harvest_interrupt_count" IS DISTINCT FROM 100;
     `);
   }
 
@@ -30,12 +39,6 @@ export class UpdateLimitHarvestForUser1766148942403 implements MigrationInterfac
     await queryRunner.query(`
       ALTER TABLE "user_clan_stats"
       ALTER COLUMN "harvest_interrupt_count" SET DEFAULT 0;
-    `);
-
-    await queryRunner.query(`
-      UPDATE "user_clan_stats"
-      SET "harvest_count" = 0,
-          "harvest_interrupt_count" = 0;
     `);
   }
 }
