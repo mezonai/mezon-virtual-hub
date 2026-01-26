3c210b42856a358a08e39ec0be28e6e4f189012d
tuockhuutien-blip <tuoc.khuutien@ncc.asia>
2026-01-26 17:26:31 +0700

    fixed-weekly-reward-clan

--- src/modules/user-clan-stat/weekly-reset.service.ts
+++ src/modules/user-clan-stat/weekly-reset.service.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { UserClanStatService } from './user-clan-stat.service';
 import { RewardManagementService } from '@modules/admin/reward/reward.service';
-import { ClanService } from '@modules/clan/clan.service';
 
 @Injectable()
 export class WeeklyResetService {
   constructor(
     private readonly userClanStatService: UserClanStatService,
     private readonly rewardManagementService: RewardManagementService,
-    private readonly clanService: ClanService,
   ) {}
 
   @Cron('0 0 * * 1', { timeZone: 'Asia/Ho_Chi_Minh' }) // mỗi thứ 2 lúc 00:00
@@ -17,6 +15,5 @@ export class WeeklyResetService {
     await this.rewardManagementService.rewardWeeklyTopMembers();
     await this.rewardManagementService.rewardWeeklyTopClans();
     await this.userClanStatService.resetWeeklyScores();
-    await this.clanService.resetWeeklyScores();
   }
 }
